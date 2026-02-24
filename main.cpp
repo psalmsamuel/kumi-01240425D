09b2f0f6b8dedfb675df81c376f43385dcc11e54
psalmsamuel <psalmsamuel23@gmail.com>
2026-02-24 02:05:18 +0000

    Fix switch/case braces and appliance class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 # include <iostream>
 #include <string>
+#include <iomanip>
+#include <fstream>
 
 using namespace std;
 class appliace {
@@ -26,9 +28,31 @@ class appliace {
         } while (hours < 0 || hours > 24);
     }
 
+    double calculateEnergy() const {
+        return (power * hours) / 1000.0;
+    }
+
     void display() const {
         cout <<"name: "<< name <<" | power: " << power << "W" <<"| hours: " <<
-        hours << endl;
+        hours <<  " | energy: " << calculateEnergy() << "KWh\n" << endl;
+        
+         
+    }
+
+    string getName() const {
+        return name;
+    }
+    double getPower() const {
+        return power;
+    }
+    double getHours() const {
+        return hours;
+    }
+
+    void setData(string n, double p, double h) {
+        name = n;
+        power = p;
+        hours = h;
     }
 };
 
@@ -40,12 +64,20 @@ int main () {
         cout << "\n=== electrical load monitoring system ===\n";
         cout <<"1. register appliance\n";
         cout <<"2. view appliances\n";
+        cout <<"3. calculate total energy consumption\n";
+        cout <<"4. calculate bill\n";
+        cout <<"5. save to file\n";
+        cout <<"6. load from file\n";
         cout <<"0. exit\n";
         cout <<"enter choice: ";
         cin >> choice;
 
         switch (choice) {
             case 1:
+                if (count >= 100) {
+                    cout <<"maximum appliance limit reached. cannot register more.\n";
+                    break;
+                }
                 appliaces[count].input();
                 count++;
                 cout <<"appliance registered successfully!\n";
@@ -61,6 +93,75 @@ int main () {
                 } 
                 break;
 
+            case 3: {
+                if (count == 0){
+                    cout <<"no appliance registered.\n";
+                } else {
+                    double
+                    totalenergy = 0;
+                    for (int i = 0; i < count; i++) { 
+                    totalenergy += appliaces[i].calculateEnergy();
+                    }
+                    cout << fixed << setprecision(2);
+                                                      cout <<"total daily energy consumption: "
+                                                      << 
+                                                      totalenergy << "KWh\n";
+                }
+                break;
+
+                case 4: {
+                    if (count == 0) {
+                        cout <<"no appliance registered.\n";
+                        break;
+                    }
+                    double tariff;
+                    do {
+                        cout <<"enter tariff rate (per KWh): ";
+                        cin >> tariff;
+                        if (tariff <= 0)
+                            cout <<"tariff must be greater than 0. \n";
+                    } while (tariff <= 0);
+                    double totalenergy = 0;
+                    for (int i = 0; i < count; i++) {
+                        totalenergy += appliaces[i].calculateEnergy();
+
+                    }
+                    double bill = totalenergy * tariff;
+                    cout << fixed << setprecision(2);
+                    cout <<"\n===== bill summary =====\n";
+                    cout <<"total energy consumption: " << totalenergy << " KWh\n";
+                    cout <<"tariff rate: KShs. " << tariff << " per KWh\n";
+                    cout <<"total bill: KShs. " << bill << endl;
+                    break;
+                }
+
+                case 5: {
+                    ofstream
+                    file("appliances.txt");
+                    for(int i = 0; i < count; i++) {
+                        file << appliaces[i].getName() << " "
+                             << appliaces[i].getPower() << " "
+                             << appliaces[i].getHours() << endl;
+                    }
+                    file.close();
+                    cout <<"appliances saved to appliances.txt\n";
+                    break;
+                }
+
+            case 6: {
+                ifstream file("appliances.txt");
+
+                string name;
+                double power, hours;
+                count = 0;
+                while (file >> name >> power >> hours) {
+                    appliaces[count].setData(name, power, hours);
+                    count++;
+                }
+                file.close();
+                cout <<"data loaded successfully!\n";
+                break;
+            }
             case 0:
                 cout << "exiting....\n";
                 break;
